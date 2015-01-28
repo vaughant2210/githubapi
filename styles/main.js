62a5e3c7337f5a37a148fe915a20f2a0e963965b
Vaughan Thandroyen <vaughant2210@gmail.com>
2015-01-28 17:18:19 -0500

    second ajax get added

--- styles/main.js
+++ styles/main.js
@@ -1,36 +1,96 @@
+(function(){
+  'use strict';
+
+
+
+
+  var githubUrl = "https://api.github.com/users/vaughant2210";
+
+
+
 $(document).ready(function(){
+  if(typeof window.token !== 'undefined'){
+    $.ajaxSetup({
+      headers: { 'Authorization': 'token ' + window.token }
+    });
+  }
 
 
-  var myGitHubAddress = "https://api.github.com/users/vaughant2210";
-
-  var githubUrl = myGitHubAddress + "?access_token=" + window.token;
+  var $list = $('.mainContainer');
 
 
+//function one
 
+function output(r){
+  console.log(r);
+
+  var sidebarUpdate = renderTemplate('sidebar', {
+    image: r.avatar_url,
+    name: r.name,
+    username: r.login,
+    joined: moment(r.created_at).fromNow(),
+  });
+
+  $list.append(sidebarUpdate);      //append to the HTML the rendered info
+}
+
+//function two
+var repoTemplate = _.template($('[data-template-name=repo]').text())
+var $repositoriesUl = $('.repositories');
+
+
+$.ajax(githubUrl + "/repos").done(function(repos){
+    console.log(repos);
+  _.each(repos, function(repo) {
+    var a={
+      name: repo.name,
+      pushed_at: moment(repo.pushed_at).fromNow(),
+      language: repo.language,
+      stargazers_count: repo.stargazers_count,
+      forks_count: repo.forks_count,
+      stargazers_url: repo.stargazers_url,
+      forks_url: repo.forks_url
+    };
+
+
+    $repositoriesUl.append(repoTemplate(a));
+  });
+});
+
+
+
+//ajax calls
 
   $.ajax({
     url: githubUrl
-    
-  });
+  }).done(function(data){
+    output(data);
+    });
+
+
+
+  // $.ajax({
+  //   url: githubUrl + "/repos"
+  // }).done(function(repos){
+  //   console.log(repos);
+  //
+  // });
 
 
 
 
 
+//Template
 
-
-
-
-
-
-
-
-
-
-
-
-
+  function renderTemplate(name, data) {
+    var $template = $('[data-template-name=' + name + ']').text();
+    $.each(data, function(prop, value) {
+      $template = $template.replace('<% ' + prop + ' %>', value);
+    });
+    return $template;
+  }
 
 
 
 });
+})();
